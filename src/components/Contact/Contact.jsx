ab4dc86ed90ad089afaa93b093be29fa3315e090
manikkalotra <pb06manik@gmail.com>
2025-05-05 10:17:46 +0530

    first commit

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,18 +13,18 @@ export const Contact = () => {
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:myemail@email.com">myemail@email.com</a>
+          <a href="https://mail.google.com/mail/?view=cm&fs=1&to=pb06manik@gmail.com">pb06manik@gmail.com</a>
         </li>
         <li className={styles.link}>
           <img
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/myname">linkedin.com/myname</a>
+          <a href="https://www.linkedin.com/in/manik-kalotra-4838a7264">linkedin.com/in/manik-kalotra-4838a7264</a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://www.github.com/myname">github.com/myname</a>
+          <a href="https://github.com/Manikkalotra">github.com/Manikkalotra</a>
         </li>
       </ul>
     </footer>
